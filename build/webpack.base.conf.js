0e84c818cae39a9fda96cc3c812da8f5aca8ede3
Bob-huang-gdut <3570411064@qq.com>
2017-06-13 11:58:24 +0800

    小更新

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -1,23 +1,47 @@
-var path = require('path')
-var utils = require('./utils')
-var config = require('../config')
-var webpack = require("webpack")
-var vueLoaderConfig = require('./vue-loader.conf')
+/*! =======================================================
+                      VERSION  1.1.0
+ * ========================================================
+ * webpack.base.config.js
+ * ========================================================
+ * 本文件为webpack基本配置文件
+ * ======================================================== */
+let path = require('path'),
+    utils = require('./utils'),
+    config = require('../config'),
+    webpack = require("webpack"),
+    vueLoaderConfig = require('./vue-loader.conf')
+
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'src');
 
 function resolve(dir) {
   return path.join(__dirname, '..', dir)
 }
 
 module.exports = {
+  // 文件入口 entry
   entry: {
+    // app入口配置
     app: './src/main.js'
   },
+  // 输出文件夹路径以及可被分块输出路径和名字
   output: {
+    // 输出文件路径
     path: config.build.assetsRoot,
+    // 输出文件名字
     filename: '[name].js',
+    // 公共路径输出配置
     publicPath: process.env.NODE_ENV === 'production' ?
       config.build.assetsPublicPath : config.dev.assetsPublicPath
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin('common.js'),
+    // 全局变量，无需每个模块引入
+    new webpack.ProvidePlugin({
+      jQuery: "jquery",
+      $: "jquery"
+    })
+  ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
@@ -28,50 +52,43 @@ module.exports = {
       'jquery': "jquery/src/jquery"
     }
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common.js'),
-    new webpack.ProvidePlugin({
-      jQuery: "jquery",
-      $: "jquery"
-    })
-  ],
   module: {
     rules: [{
-        test: /\.vue$/,
-        loader: 'vue-loader',
-        // options: vueLoaderConfig
-        options: {
-          loaders: {
-            scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
-            sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
-          }
-        }
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        include: [resolve('src'), resolve('test')]
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
-        }
-      },
-      {
-        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      test: /\.vue$/,
+      loader: 'vue-loader',
+      // options: vueLoaderConfig
+      options: {
+        loaders: {
+          scss: 'vue-style-loader!css-loader!sass-loader', // <style lang="scss">
+          sass: 'vue-style-loader!css-loader!sass-loader?indentedSyntax' // <style lang="sass">
         }
       }
+    },
+    {
+      test: /\.json$/,
+      loader: 'json'
+    },
+    {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [resolve('src'), resolve('test')]
+    },
+    {
+      test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('img/[name].[hash:7].[ext]')
+      }
+    },
+    {
+      test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+      loader: 'url-loader',
+      options: {
+        limit: 10000,
+        name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+      }
+    }
     ]
   }
 }
